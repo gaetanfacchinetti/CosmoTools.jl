8bd3f051f2e6060394b484b5b1aa5ad84bfbdbeb
Gaétan Facchinetti <gaetanfacc@gmail.com>
2024-05-24 18:44:17 +0200

    Adding Eddington's inversion method - not checked

--- src/Halos.jl
+++ src/Halos.jl
@@ -20,9 +20,9 @@
 export Halo, nfwProfile, αβγProfile, HaloProfile, coreProfile, plummerProfile
 export halo_from_mΔ_and_cΔ, halo_from_mΔ
 export mΔ_from_ρs_and_rs, mΔ, rΔ_from_ρs_and_rs, rΔ, cΔ_from_ρs, cΔ, ρ_halo, μ_halo, m_halo
-export velocity_dispersion, gravitational_potential, escape_velocity, orbital_frequency, circular_velocity
-export gravitational_potential_kms
-export der_1_ρ_halo, der_2_ρ_halo, der_1_gravitational_potential
+export velocity_dispersion, ψ_halo, escape_velocity, orbital_frequency, circular_velocity
+export ψ_halo_kms
+export der_1_ρ_halo, der_2_ρ_halo, der_1_ψ_halo
 
 abstract type HaloProfile{T<:Real} end
 
@@ -63,10 +63,16 @@ end
 
 # dimensionless properties of the halo
 # use analytical expression when possible
-function gravitational_potential(x::Real, xt::Real, p::αβγProfile = nfwProfile) 
+function ψ_halo(x::Real, xt::Real, p::αβγProfile = nfwProfile) 
     (p == nfwProfile) && ( (x > 0) && return (log(1+x)/x -log(1+xt)/xt))  && ( (x == 0) && return (1 -log(1+xt)/xt))
     return - QuadGK.quadgk(lnxp -> μ_halo(exp(lnxp), p) / exp(lnxp), x, xt, rtol=1e-5)[1] 
 end
+
+
+#-------------
+#
+# Usefull functions for Eddington's inversion
+# 
 
 # first derivative of the halo mass density
 der_1_ρ_halo(x::Real, p::αβγProfile = nfwProfile) = -x^(-1-p.γ) * (1+x^p.α)^((p.α + p.β - p.γ)/p.α) * (p.β * x^(p.α) + p.γ)
@@ -75,9 +81,35 @@ der_1_ρ_halo(x::Real, p::αβγProfile = nfwProfile) = -x^(-1-p.γ) * (1+x^p.α
 der_2_ρ_halo(x::Real, p::αβγProfile = nfwProfile) = x^(-2-p.γ) * (1+x^p.α)^((-2 * p.α-p.β+p.γ)/p.α) * (x^(2 * p.α) * p.β *  (1+p.β)+p.γ+p.γ^2+x^p.α * (p.β-p.α * p.β+p.γ+p.α * p.γ+ 2 * p.β * p.γ))
 
 # derivative of the gravitational potential with respect to the mass
-der_1_gravitational_potential(x::Real, xt::Real, p::HaloProfile = nfwProfile) = -μ_halo(x, p) / x^2
+der_1_ψ_halo(x::Real, p::HaloProfile = nfwProfile) = -μ_halo(x, p) / x^2
 
+# second derivative of the density profile with respect to the gravitational potential
+function der_2_ρ_halo_vs_ψ(x::Real, p::HaloProfile = nfwProfile)
+  m = μ_halo(x, p)
+  return (2 * x^3) / m^2 * ( (1 - x^3 * ρ_halo(x, p)  / (2 * m)) * der_1_ρ_halo(x, p) + (x / 2) * der_2_ρ_halo(x, p)  );
+end 
 
+# for the Eddington inversion method we define φ = ψ/E
+# for a given value of E and φ we must get the associated position x
+function x_vs_φ(φ::Real, E::Real, x0::Real, p::HaloProfile = nfwProfile)
+  _to_solve(x::Real) = ψ_halo(x, p) - E * φ
+  x = Roots.find_zero(_to_solve, (0, 1.1*x0), Roots.Bisection(), xrtol=1e-5)                     # first 'rough' estimate
+  return Roots.find_zero((_to_solve, xp->der_1_ψ_halo(xp, p)), x, Roots.Newton(),  xrtol=1e-10) # precise estimate with Newton's method
+end
+
+# Need to be checked
+function phase_space_Eddington(E::Real, x0::Real, p::HaloProfile = nfwProfile; remove_divergence = false) 
+  
+  # may need to improve the integral computation with methods like double logarithms
+  _to_integrate(φ::Real) = der_2_ρ_halo_vs_ψ(x_vs_φ(φ, E, x0, p), p) / sqrt(1-φ)
+  integral = QuadGK.quadgk(_to_integrate, 0, 1, rtol=1e-3)
+  
+  div = 0 # diverging term (non physical)
+  !(remove_divergence) && (div = - x0^2 * der_1_ρ_halo(x0, p) / μ_halo(x0, p))
+  return 1/(sqrt(8) * π^2) * ( div / sqrt(E) + sqrt(E) * integral )
+
+end 
+#-------------
 
 
 function velocity_dispersion(x::Real, xt::Real, p::αβγProfile = nfwProfile, approx::Bool = true) 
@@ -119,7 +151,7 @@ end
 ## Definition of relationships between concentration, mass, scale density and scale radius
 function cΔ_from_ρs(ρs::Real, hp::HaloProfile{<:Real}, Δ::Real, ρ_ref::Real)
     g(c::Real) = c^3 / μ_halo(c, hp) - 3 * ρs / Δ / ρ_ref
-    Roots.find_zero(g, (1e-10, 1e+10), Roots.Bisection()) 
+    return Roots.find_zero(g, (1e-10, 1e+10), Roots.Bisection()) 
 end
 
 cΔ_from_ρs(ρs::Real, hp::HaloProfile{<:Real} = nfwProfile, Δ::Real=200, cosmo::Cosmology = plnack18) = cΔ_from_ρs(ρs, hp, Δ, cosmo.bkg.ρ_c0)
@@ -171,16 +203,16 @@ velocity_dispersion(r::Real, rt::Real, h::Halo) = sqrt(4 * π * h.ρs * h.rs^2 *
 velocity_dispersion_kms(r::Real, rt::Real, h::Halo) = velocity_dispersion(r, rt, h) * MPC_TO_KM
 
 """ gravitational potential in (Mpc / s)^2 """
-gravitational_potential(r::Real, rt::Real, h::Halo) = - 4 * π * h.ρs * h.rs^2 * G_NEWTON * gravitational_potential(r/h.rs, rt/h.rs, h.hp) 
+ψ_halo(r::Real, rt::Real, h::Halo) = - 4 * π * h.ρs * h.rs^2 * G_NEWTON * ψ_halo(r/h.rs, rt/h.rs, h.hp) 
 
 """ gravitational potential in (km / s)^2 """
-gravitational_potential_kms(r::Real, rt::Real, h::Halo) = gravitational_potential(r, rt, h) * (MPC_TO_KM^2)
+ψ_halo_kms(r::Real, rt::Real, h::Halo) = ψ_halo(r, rt, h) * (MPC_TO_KM^2)
 
 """ escape velocity in (Mpc / s) """
-escape_velocity(r::Real, rt::Real, h::Halo) = sqrt(2*abs(gravitational_potential(r, rt, h)))
+escape_velocity(r::Real, rt::Real, h::Halo) = sqrt(2*abs(ψ_halo(r, rt, h)))
 
 """ escape velocity in (km / s) """
-escape_velocity_kms(r::Real, rt::Real, h::Halo) = sqrt(2*abs(gravitational_potential_kms(r, rt, h))) * MPC_TO_KM
+escape_velocity_kms(r::Real, rt::Real, h::Halo) = sqrt(2*abs(ψ_halo_kms(r, rt, h))) * MPC_TO_KM
 
 """ circular velocity in (Mpc / s)"""
 circular_velocity(r::Real, h::Halo) = sqrt(G_NEWTON * m_halo(r, h) / r)
